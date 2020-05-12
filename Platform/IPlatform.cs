1d3a8f555ada23e41a94c69259bac5e2df99651c
Robert Roose <robert@shivercube.com>
2020-05-11 21:59:31 -0300

    Moved LoadServers into platform specific class

---
+++ Platform/IPlatform.cs
@@ -0,0 +1,20 @@
+﻿using agrix.Configuration;
+using System.Collections.Generic;
+using YamlDotNet.RepresentationModel;
+
+namespace agrix.Platform
+{
+    /// <summary>
+    /// Represents a platform that can be configured via code.
+    /// </summary>
+    internal interface IPlatform
+    {
+        /// <summary>
+        /// Loads all the server configurations from the given YAML.
+        /// </summary>
+        /// <param name="config">The YAML list to load servers from.</param>
+        /// <returns>The list of Server configurations loaded from the given
+        /// YAML.</returns>
+        public IList<Server> LoadServers(YamlSequenceNode serverItems);
+    }
+}
