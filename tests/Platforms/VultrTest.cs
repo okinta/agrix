d97caa43e078ea2fb82a99977670bd3a886af383
Robert Roose <robert@shivercube.com>
2020-05-12 16:27:15 -0300

    Added tests for GetRegionID and GetPlanID

--- tests/Platforms/VultrTest.cs
+++ tests/Platforms/VultrTest.cs
@@ -1,14 +1,26 @@
-﻿using Xunit;
+﻿using agrix.Configuration;
+using Xunit;
 
 namespace tests.Platforms
 {
     public class VultrTest
     {
-        [Fact]
-        public void TestGetRegionID()
+        [Theory]
+        [InlineData(1, "New Jersey")]
+        [InlineData(40, "Singapore")]
+        public void TestGetRegionID(int id, string name)
         {
             var vultr = new agrix.Platforms.Vultr(Settings.Default.VultrApiKey);
-            Assert.Equal(1, vultr.GetRegionID("New Jersey"));
+            Assert.Equal(id, vultr.GetRegionID(name));
+        }
+
+        [Theory]
+        [InlineData(204, 4, 8192, "SSD")]
+        [InlineData(404, 4, 16384, "HIGHFREQUENCY")]
+        public void TestGetPlanID(int id, int cpu, int ram, string type)
+        {
+            var vultr = new agrix.Platforms.Vultr(Settings.Default.VultrApiKey);
+            Assert.Equal(id, vultr.GetPlanID(new Plan(cpu, ram, type)));
         }
     }
 }
