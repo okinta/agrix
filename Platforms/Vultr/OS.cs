d9da5ac1a6a3493ae917bca0d6d62e827f363caf
Robert Roose <robert@shivercube.com>
2020-05-12 21:14:08 -0300

    Implemented Vultr OS logic

---
+++ Platforms/Vultr/OS.cs
@@ -0,0 +1,166 @@
+﻿using OperatingSystem = Vultr.API.Models.Responses.OperatingSystem;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Vultr.API.Models.Responses;
+using Vultr.API;
+
+namespace agrix.Platforms.Vultr
+{
+    /// <summary>
+    /// Represents a Vultr Operating System configuration.
+    /// </summary>
+    internal struct OS
+    {
+        public int OSID { get; }
+        public int? APPID { get; }
+        public int? ISOID { get; }
+        public int? SCRIPTID { get; }
+        public int? SNAPSHOTID { get; }
+
+        private const int AppOSID = 186;
+        private const int ISOOSID = 159;
+        private const int SnapshotOSID = 164;
+
+        private OS(int osid, int? appid = null, int? isoid = null,
+            int? scriptid = null, int? snapshotid = null)
+        {
+            OSID = osid;
+            APPID = appid;
+            ISOID = isoid;
+            SCRIPTID = scriptid;
+            SNAPSHOTID = snapshotid;
+        }
+
+        /// <summary>
+        /// Creates a new application configuration.
+        /// </summary>
+        /// <param name="name">The name of the application.</param>
+        /// <param name="client">The VultrClient instance to communicate with
+        /// Vultr.</param>
+        /// <returns>The configured OS instance.</returns>
+        public static OS CreateApp(string name, VultrClient client)
+        {
+            var apps = client.Application.GetApplications();
+
+            KeyValuePair<string, Application> app;
+            try
+            {
+                app = apps.Applications.Single(app => app.Value.deploy_name == name);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new ArgumentException(
+                    string.Format("Cannot find app called {0}", name), "name", e);
+            }
+
+            return new OS(AppOSID, appid: int.Parse(app.Key));
+        }
+
+        /// <summary>
+        /// Creates a new ISO configuration.
+        /// </summary>
+        /// <param name="name">The name of the ISO.</param>
+        /// <param name="client">The VultrClient instance to communicate with
+        /// Vultr.</param>
+        /// <returns>The configured OS instance.</returns>
+        public static OS CreateISO(string name, VultrClient client)
+        {
+            var isos = client.ISOImage.GetISOImages();
+
+            KeyValuePair<string, ISOImage> iso;
+            try
+            {
+                iso = isos.ISOImages.Single(iso => iso.Value.filename == name);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new ArgumentException(
+                    string.Format("Cannot find ISO called {0}", name), "name", e);
+            }
+
+            return new OS(ISOOSID, isoid: int.Parse(iso.Key));
+        }
+
+        /// <summary>
+        /// Creates a new Operating System configuration.
+        /// </summary>
+        /// <param name="name">The name of the Operating System.</param>
+        /// <param name="client">The VultrClient instance to communicate with
+        /// Vultr.</param>
+        /// <returns>The configured OS instance.</returns>
+        public static OS CreateOS(string name, VultrClient client)
+        {
+            return new OS(FindOperatingSystem(name, client));
+        }
+
+        /// <summary>
+        /// Creates a new script configuration.
+        /// </summary>
+        /// <param name="name">The name of the Operating System.</param>
+        /// <param name="scriptName">The name of the script.</param>
+        /// <param name="client">The VultrClient instance to communicate with
+        /// Vultr.</param>
+        /// <returns>The configured OS instance.</returns>
+        public static OS CreateScript(string name, string scriptName, VultrClient client)
+        {
+            var scripts = client.StartupScript.GetStartupScripts();
+
+            KeyValuePair<string, StartupScript> script;
+            try
+            {
+                script = scripts.StartupScripts.Single(
+                    system => system.Value.name == scriptName);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new ArgumentException(
+                    string.Format("Cannot find script called {0}", scriptName),
+                    "scriptName", e);
+            }
+
+            return new OS(
+                FindOperatingSystem(name, client), scriptid: int.Parse(script.Key));
+        }
+
+        /// <summary>
+        /// Creates a new snapshot configuration.
+        /// </summary>
+        /// <param name="id">The ID of the snapshot.</param>
+        /// <param name="client">The VultrClient instance to communicate with
+        /// Vultr.</param>
+        /// <returns>The configured OS instance.</returns>
+        public static OS CreateSnapshot(int id, VultrClient client)
+        {
+            var snapshots = client.Snapshot.GetSnapshots();
+
+            if (!snapshots.Snapshots.ContainsKey(id.ToString()))
+            {
+                throw new ArgumentException(
+                    string.Format("Cannot find snapshot with ID {0}", id), "id");
+            }
+
+            return new OS(SnapshotOSID, snapshotid: id);
+        }
+
+        private static int FindOperatingSystem(string name, VultrClient client)
+        {
+            var systems = client.OperatingSystem.GetOperatingSystems();
+
+            KeyValuePair<int, OperatingSystem> system;
+            try
+            {
+                system = systems.OperatingSystems.Single(
+                    system => system.Value.name == name);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new ArgumentException(
+                    string.Format("Cannot find Operating System called {0}", name),
+                    "name", e);
+            }
+
+            return system.Key;
+        }
+    }
+}
---
+++ tests/Platforms/Vultr/OSTest.cs
@@ -0,0 +1,68 @@
+﻿using agrix.Platforms.Vultr;
+using Vultr.API;
+using Xunit;
+
+namespace tests.Platforms.Vultr
+{
+    public class OSTest
+    {
+        [Theory]
+        [InlineData(37, "Docker on Ubuntu 18.04 x64")]
+        [InlineData(38, "cPanel on CentOS 7 x64")]
+        public void TestCreateApp(int id, string name)
+        {
+            var client = new VultrClient(Settings.Default.VultrApiKey);
+            var app = OS.CreateApp(name, client);
+
+            Assert.Equal(186, app.OSID);
+            Assert.Equal(id, app.APPID);
+            Assert.Null(app.ISOID);
+            Assert.Null(app.SCRIPTID);
+            Assert.Null(app.SNAPSHOTID);
+        }
+
+        [Theory]
+        [InlineData("server2019.iso")]
+        [InlineData("installcoreos.iso")]
+        public void TestCreateISO(string name)
+        {
+            var client = new VultrClient(Settings.Default.VultrApiKey);
+            var iso = OS.CreateISO(name, client);
+
+            Assert.Equal(159, iso.OSID);
+            Assert.True(iso.ISOID > 0);
+            Assert.Null(iso.APPID);
+            Assert.Null(iso.SCRIPTID);
+            Assert.Null(iso.SNAPSHOTID);
+        }
+
+        [Theory]
+        [InlineData(230, "FreeBSD 11 x64")]
+        [InlineData(240, "Windows 2016 x64")]
+        public void TestCreateOS(int id, string name)
+        {
+            var client = new VultrClient(Settings.Default.VultrApiKey);
+            var os = OS.CreateOS(name, client);
+
+            Assert.Equal(id, os.OSID);
+            Assert.Null(os.APPID);
+            Assert.Null(os.ISOID);
+            Assert.Null(os.SCRIPTID);
+            Assert.Null(os.SNAPSHOTID);
+        }
+
+        [Theory]
+        [InlineData("setup-ubuntu")]
+        public void TestCreateScript(string name)
+        {
+            var client = new VultrClient(Settings.Default.VultrApiKey);
+            var script = OS.CreateScript("FreeBSD 11 x64", name, client);
+
+            Assert.Equal(230, script.OSID);
+            Assert.Null(script.APPID);
+            Assert.Null(script.ISOID);
+            Assert.Null(script.SNAPSHOTID);
+            Assert.True(script.SCRIPTID > 0);
+        }
+    }
+}
