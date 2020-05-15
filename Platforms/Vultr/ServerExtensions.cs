20c0fc48d591369705be98372c6de6d4fcd67e71
Robert Roose <robert@shivercube.com>
2020-05-15 14:00:02 -0300

    Added ability to compare Server instances

---
+++ Platforms/Vultr/ServerExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Vultr.API.Models;
+
+namespace agrix.Platforms.Vultr
+{
+    /// <summary>
+    /// Extends the Vultr.API.Models.Server class.
+    /// </summary>
+    internal static class ServerExtensions
+    {
+        /// <summary>
+        /// Returns whether or not the two given servers are equivalent.
+        /// </summary>
+        /// <param name="server">The Server to compare <paramref name="other"/>
+        /// with.</param>
+        /// <param name="other">The Server to compare <paramref name="server"/>
+        /// with.</param>
+        /// <returns>True if the Server instances are equivalent; false
+        /// otherwise.</returns>
+        public static bool IsEquivalent(this Server server, Server other)
+        {
+            if (server is null)
+                throw new ArgumentNullException("server", "server must not be null");
+
+            if (other is null)
+                throw new ArgumentNullException("other", "other must not be null");
+
+            foreach (var property in server.GetType().GetProperties())
+            {
+                var serverValue = property.GetValue(server);
+                var otherValue = property.GetValue(other);
+                if (serverValue == otherValue) continue;
+
+                if (serverValue?.GetType() == typeof(double)
+                    && otherValue?.GetType() == typeof(double)
+                    && Math.Abs((double)serverValue - (double)otherValue) < 0.1)
+                        continue;
+
+                Console.WriteLine("Servers do not match. {0} is different.",
+                    property.Name);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ tests/Platforms/Vultr/ServerExtensionsTest.cs
@@ -0,0 +1,48 @@
+﻿using agrix.Platforms.Vultr;
+using Vultr.API.Models;
+using Xunit;
+
+namespace tests.Platforms.Vultr
+{
+    public class ServerExtensionsTest
+    {
+        [Fact]
+        public void TestIsEquivalent()
+        {
+            Assert.True(new Server()
+            {
+                APPID = "123",
+                tag = "test"
+            }.IsEquivalent(new Server()
+            {
+                APPID = "123",
+                tag = "test"
+            }));
+        }
+
+        [Fact]
+        public void TestIsNotEquivalent()
+        {
+            Assert.False(new Server()
+            {
+                APPID = "123",
+                tag = "tet"
+            }.IsEquivalent(new Server()
+            {
+                APPID = "123",
+                tag = "test"
+            }));
+
+            Assert.False(new Server()
+            {
+                APPID = "123",
+                tag = "test"
+            }.IsEquivalent(new Server()
+            {
+                APPID = "123",
+                tag = "test",
+                SUBID = "156"
+            }));
+        }
+    }
+}
