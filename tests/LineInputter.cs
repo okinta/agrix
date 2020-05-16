588fe0eb60722561325905308adbb6cf8d0254c8
Robert Roose <robert@shivercube.com>
2020-05-16 20:42:43 -0300

    Added tests for `agrix validate`

---
+++ tests/LineInputter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tests
+{
+    /// <summary>
+    /// Allows input to be streamed line by line calling the ReadLine method.
+    /// </summary>
+    internal class LineInputter
+    {
+        private Queue<string> Lines { get; }
+
+        /// <summary>
+        /// Creates a new instances to stream the given content.
+        /// </summary>
+        /// <param name="content">The content to stream line by line.</param>
+        public LineInputter(string content)
+        {
+            if (!string.IsNullOrEmpty(content))
+                Lines = new Queue<string>(
+                    content.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None));
+
+            else
+                Lines = new Queue<string>();
+        }
+
+        /// <summary>
+        /// Gets a new line of input.
+        /// </summary>
+        /// <returns>The next line of input, or null if there is no more input
+        /// left.</returns>
+        public string ReadLine()
+        {
+            Lines.TryDequeue(out var line);
+            return line;
+        }
+    }
+}
--- tests/Program/ProgramTest.cs
+++ tests/Program/ProgramTest.cs
@@ -1,5 +1,6 @@
 ﻿using AProgram = agrix.Program.Program;
 using System.Reflection;
+using tests.Properties;
 using Xunit;
 
 namespace tests.Program
@@ -18,6 +19,22 @@ namespace tests.Program
         public void TestHelp()
         {
             Assert.Equal(0, AProgram.Main(TestAssembly, ReadLine, "help"));
+        }
+
+        [Fact]
+        public void TestValidate()
+        {
+            var input = new LineInputter(Resources.TestPlatformConfig);
+            Assert.Equal(0, AProgram.Main(TestAssembly, input.ReadLine,
+                "validate", "--apikey", "abc"));
+        }
+
+        [Fact]
+        public void TestValidateInvalid()
+        {
+            var input = new LineInputter("what is this nonsense");
+            Assert.Equal(1, AProgram.Main(TestAssembly, input.ReadLine,
+                "validate", "--apikey", "abc"));
         }
 
         private string ReadLine() { return null; }
