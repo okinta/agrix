8aa9249fa93966d08699e1070e4f45924abcc9f7
Robert Roose <robert@shivercube.com>
2020-05-16 21:25:20 -0300

    Added test for FirewallParser

--- tests/Configuration/InfrastructureConfigurationTest.cs
+++ tests/Configuration/InfrastructureConfigurationTest.cs
@@ -1,10 +1,6 @@
-﻿using agrix.Configuration.Parsers;
-using agrix.Configuration;
-using agrix.Extensions;
-using agrix.Platforms.Vultr;
+﻿using agrix.Platforms.Vultr;
 using agrix;
 using System.Text;
-using System;
 using tests.Properties;
 using Xunit;
 
@@ -13,59 +9,6 @@ namespace tests.Configuration
     // TODO: Rename
     public class InfrastructureConfigurationTest : BaseTest
     {
-        [Fact]
-        public void TestLoadFirewalls()
-        {
-            var node = LoadYaml(Resources.FirewallsConfig).GetSequence("firewalls");
-
-            var firewalls = new Parser().Load(
-                "firewalls",
-                node,
-                new FirewallParser().Parse);
-
-            Assert.Equal(2, firewalls.Count);
-
-            var firewall = firewalls[0];
-            Assert.Equal("ssh", firewalls[0].Name);
-            Assert.Equal(2, firewalls[0].Rules.Count);
-
-            var rule = firewall.Rules[0];
-            Assert.Equal(IPType.V4, rule.IPType);
-            Assert.Equal(Protocol.TCP, rule.Protocol);
-            Assert.Equal("", rule.Source);
-            Assert.Equal("0.0.0.0", rule.Subnet);
-            Assert.Equal(0, rule.SubnetSize);
-            Assert.Equal("22", rule.Ports);
-
-            rule = firewall.Rules[1];
-            Assert.Equal(IPType.V4, rule.IPType);
-            Assert.Equal(Protocol.TCP, rule.Protocol);
-            Assert.Equal("", rule.Source);
-            Assert.Equal("0.0.0.0", rule.Subnet);
-            Assert.Equal(0, rule.SubnetSize);
-            Assert.Equal("3389", rule.Ports);
-
-            firewall = firewalls[1];
-            Assert.Equal("myapp", firewall.Name);
-            Assert.Equal(2, firewall.Rules.Count);
-
-            rule = firewall.Rules[0];
-            Assert.Equal(IPType.V4, rule.IPType);
-            Assert.Equal(Protocol.UDP, rule.Protocol);
-            Assert.Equal("", rule.Source);
-            Assert.Equal("172.0.24.1", rule.Subnet);
-            Assert.Equal(20, rule.SubnetSize);
-            Assert.Equal("8000:8100", rule.Ports);
-
-            rule = firewall.Rules[1];
-            Assert.Equal(IPType.V4, rule.IPType);
-            Assert.Equal(Protocol.TCP, rule.Protocol);
-            Assert.Equal("cloudflare", rule.Source);
-            Assert.Equal("", rule.Subnet);
-            Assert.Null(rule.SubnetSize);
-            Assert.Equal("80", rule.Ports);
-        }
-
         [Fact]
         public void TestLoadPlatform()
         {
