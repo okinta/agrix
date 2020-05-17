a0528574db4827afe0dbecd09afe4ef04f0d80b7
Robert Roose <robert@shivercube.com>
2020-05-17 18:20:43 -0300

    Added tests for VultrServerProvisioner

--- tests/Platforms/Vultr/Provisioners/VultrServerProvisionerTest.cs
+++ tests/Platforms/Vultr/Provisioners/VultrServerProvisionerTest.cs
@@ -1,6 +1,7 @@
 ﻿using agrix.Configuration;
 using agrix.Platforms.Vultr.Provisioners;
-using Vultr.API;
+using tests.Properties;
+using tests.TestHelpers;
 using Xunit;
 
 namespace tests.Platforms.Vultr.Provisioners
@@ -11,13 +12,137 @@ namespace tests.Platforms.Vultr.Provisioners
 
         private const string Region = "New Jersey";
 
-        private static VultrServerProvisioner Platform
+        /// <summary>
+        /// Tests that VultrPlatform.Provision(Server) stands up a basic server.
+        /// </summary>
+        [Fact]
+        public void TestProvisionServer()
         {
-            get
-            {
-                var client = new VultrClient(Settings.Default.VultrApiKey);
-                return new VultrServerProvisioner(client);
-            }
+            var server = new Server(
+                new OperatingSystem(name: "Fedora 32 x64"),
+                Plan, Region, userData: "test");
+
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/os/list", Resources.VultrOSList),
+                new CustomMockHttpHandler("/regions/list?availability=yes",
+                    Resources.VultrRegionsList),
+                new CustomMockHttpHandler("/plans/list?type=all",
+                    Resources.VultrPlansList),
+                new CustomMockHttpHandler("/server/list"),
+                new CustomMockHttpHandler("/server/create",
+                    "DCID=1&VPSPLANID=201&OSID=389&enable_private_network=no&userdata=dGVzdA%3D%3D&notify_activate=no",
+                    "{\"SUBID\": \"1312965\"}")
+            );
+            new VultrServerProvisioner(requests.Client).Provision(server);
+            requests.AssertAllCalledOnce();
+        }
+
+        /// <summary>
+        /// Tests that VultrPlatform.Provision(Server, dryrun: true) doesn't stand up
+        /// a basic server.
+        /// </summary>
+        [Fact]
+        public void TestProvisionServerDryRun()
+        {
+            var server = new Server(
+                new OperatingSystem(name: "Fedora 32 x64"),
+                Plan, Region);
+
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/os/list", Resources.VultrOSList),
+                new CustomMockHttpHandler("/regions/list?availability=yes",
+                    Resources.VultrRegionsList),
+                new CustomMockHttpHandler("/plans/list?type=all",
+                    Resources.VultrPlansList),
+                new CustomMockHttpHandler("/server/list")
+            );
+            new VultrServerProvisioner(requests.Client).Provision(server, true);
+            requests.AssertAllCalledOnce();
+        }
+
+        /// <summary>
+        /// Tests that VultrPlatform.Provision(Server) updates an existing server.
+        /// </summary>
+        [Fact]
+        public void TestProvisionUpdateServer()
+        {
+            var server = new Server(
+                new OperatingSystem(name: "Fedora 32 x64"),
+                Plan, Region, label: "my new server"
+            );
+
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/os/list", Resources.VultrOSList),
+                new CustomMockHttpHandler("/regions/list?availability=yes",
+                    Resources.VultrRegionsList),
+                new CustomMockHttpHandler("/plans/list?type=all",
+                    Resources.VultrPlansList),
+                new CustomMockHttpHandler(
+                    "/server/list", Resources.VultrServerList),
+                new CustomMockHttpHandler(
+                    "/server/destroy", "SUBID=576965", ""),
+                new CustomMockHttpHandler("/server/create",
+                    "DCID=1&VPSPLANID=201&OSID=389&enable_private_network=no&label=my+new+server&notify_activate=no",
+                    "{\"SUBID\": \"1312965\"}")
+            );
+            new VultrServerProvisioner(requests.Client).Provision(server);
+            requests.AssertAllCalledOnce();
+        }
+
+        /// <summary>
+        /// Tests that VultrPlatform.Provision(Server, dryrun: true) doesn't update an
+        /// existing server.
+        /// </summary>
+        [Fact]
+        public void TestProvisionUpdateServerDryrun()
+        {
+            var server = new Server(
+                new OperatingSystem(name: "Fedora 32 x64"),
+                Plan, Region, label: "my new server"
+            );
+
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/os/list", Resources.VultrOSList),
+                new CustomMockHttpHandler("/regions/list?availability=yes",
+                    Resources.VultrRegionsList),
+                new CustomMockHttpHandler("/plans/list?type=all",
+                    Resources.VultrPlansList),
+                new CustomMockHttpHandler(
+                    "/server/list", Resources.VultrServerList)
+            );
+            new VultrServerProvisioner(requests.Client).Provision(server, true);
+            requests.AssertAllCalledOnce();
+        }
+
+        /// <summary>
+        /// Tests that VultrPlatform.Provision(Server) doesn't change a server that
+        /// already matches what the configuration defines.
+        /// </summary>
+        [Fact]
+        public void TestProvisionDoNotUpdateUnchangedServer()
+        {
+            var server = new Server(
+                new OperatingSystem(name: "CentOS 6 x64"),
+                new Plan(2, 4096, "SSD"),
+                Region, label: "my new server", tag: "mytag"
+            );
+
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/os/list", Resources.VultrOSList),
+                new CustomMockHttpHandler("/regions/list?availability=yes",
+                    Resources.VultrRegionsList),
+                new CustomMockHttpHandler("/plans/list?type=all",
+                    Resources.VultrPlansList),
+                new CustomMockHttpHandler(
+                    "/server/list", Resources.VultrServerList)
+            );
+            new VultrServerProvisioner(requests.Client).Provision(server);
+            requests.AssertAllCalledOnce();
         }
 
         [Theory]
@@ -25,7 +150,12 @@ namespace tests.Platforms.Vultr.Provisioners
         [InlineData(40, "Singapore")]
         public void TestGetRegionId(int id, string name)
         {
-            Assert.Equal(id, Platform.GetRegionId(name));
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/regions/list", Resources.VultrRegionsList));
+
+            var provisioner = new VultrServerProvisioner(requests.Client);
+            Assert.Equal(id, provisioner.GetRegionId(name));
         }
 
         [Theory]
@@ -33,17 +163,27 @@ namespace tests.Platforms.Vultr.Provisioners
         [InlineData(404, 4, 16384, "HIGHFREQUENCY")]
         public void TestGetPlanId(int id, int cpu, int ram, string type)
         {
-            Assert.Equal(id, Platform.GetPlanId(
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/plans/list", Resources.VultrPlansList));
+
+            var provisioner = new VultrServerProvisioner(requests.Client);
+            Assert.Equal(id, provisioner.GetPlanId(
                 new Plan(cpu, ram, type)));
         }
 
         [Fact]
         public void TestGetOsApp()
         {
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/app/list", Resources.VultrAppList));
+
+            var provisioner = new VultrServerProvisioner(requests.Client);
             var server = new Server(
-                new OperatingSystem(app: "LAMP on CentOS 7 x64"),
+                new OperatingSystem("LAMP on CentOS 7 x64"),
                 Plan, Region);
-            var os = Platform.GetOs(server);
+            var os = provisioner.GetOs(server);
             Assert.Equal(186, os.OsId);
             Assert.Equal(41, os.Appid);
             Assert.Null(os.IsoId);
@@ -54,15 +194,22 @@ namespace tests.Platforms.Vultr.Provisioners
         [Fact]
         public void TestGetOsScript()
         {
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/os/list", Resources.VultrOSList),
+                new CustomMockHttpHandler(
+                    "/startupscript/list", Resources.VultrStartupScripts));
+
+            var provisioner = new VultrServerProvisioner(requests.Client);
             var server = new Server(
                 new OperatingSystem(name: "Fedora 32 x64"),
-                Plan, Region, startupScript: "setup-ubuntu");
-            var os = Platform.GetOs(server);
+                Plan, Region, startupScript: "hello-boot");
+            var os = provisioner.GetOs(server);
             Assert.Equal(389, os.OsId);
+            Assert.Equal(3, os.ScriptId);
             Assert.Null(os.Appid);
             Assert.Null(os.IsoId);
             Assert.Null(os.SnapshotId);
-            Assert.True(os.ScriptId > 0);
         }
 
         [Theory]
@@ -70,10 +217,15 @@ namespace tests.Platforms.Vultr.Provisioners
         [InlineData(366, "OpenBSD 6.6 x64")]
         public void TestGetOs(int id, string name)
         {
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/os/list", Resources.VultrOSList));
+
+            var provisioner = new VultrServerProvisioner(requests.Client);
             var server = new Server(
                 new OperatingSystem(name: name),
                 Plan, Region);
-            var os = Platform.GetOs(server);
+            var os = provisioner.GetOs(server);
             Assert.Equal(id, os.OsId);
             Assert.Null(os.Appid);
             Assert.Null(os.IsoId);
@@ -84,10 +236,15 @@ namespace tests.Platforms.Vultr.Provisioners
         [Fact]
         public void TestGetIso()
         {
+            using var requests = new MockVultrRequests(
+                new CustomMockHttpHandler(
+                    "/iso/list", Resources.VultrISOList));
+
+            var provisioner = new VultrServerProvisioner(requests.Client);
             var server = new Server(
                 new OperatingSystem(iso: "installcoreos.iso"),
                 Plan, Region);
-            var os = Platform.GetOs(server);
+            var os = provisioner.GetOs(server);
             Assert.Equal(159, os.OsId);
             Assert.Null(os.Appid);
             Assert.Null(os.ScriptId);
